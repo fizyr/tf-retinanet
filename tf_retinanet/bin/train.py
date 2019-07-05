f6469b20009b5efdf7b7f1a7b33a399fd5caf7aa
vcarpani <v.carpani@fizyr.com>
2019-07-05 16:59:49 +0200

    First fixes after review.

--- tf_retinanet/bin/train.py
+++ tf_retinanet/bin/train.py
@@ -14,7 +14,7 @@ from .. import losses
 from .. import models
 from ..backbone import get_backbone
 from ..callbacks import RedirectModel
-from ..generator import get_generators
+from ..generators import get_generators
 
 def makedirs(path): #TODO still needed?
 	# Intended behavior: try to create the directory,
@@ -26,12 +26,23 @@ def makedirs(path): #TODO still needed?
 		if not os.path.isdir(path):
 			raise
 
+def set_defaults(config):
+	if not config['callbacks']['snapshots_path']:
+		from pathlib import Path
+		home = str(Path.home())
+		config['callbacks']['snapshots_path'] = os.path.join(home, 'retinanet-snapshots')
+	if not config['callbacks']['project_name']:
+		from datetime import datetime
+		config['callbacks']['project_name'] = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+	return config
+
+
 def parse_yaml():
 	#TODO get the filename using a parser
 	with open(sys.argv[1], 'r') as stream:
 		try:
 			config = yaml.safe_load(stream)
-			return config
+			return set_defaults(config)
 		except yaml.YAMLError as exc:
 			raise(exc)
 
@@ -39,20 +50,20 @@ def parse_yaml():
 def setup_gpu(gpu_id):
 	gpus = tf.config.experimental.list_physical_devices('GPU')
 	if gpus:
-		# Restrict TensorFlow to only use the first GPU
+		# Restrict TensorFlow to only use the first GPU.
 		try:
-			# Currently, memory growth needs to be the same across GPUs
+			# Currently, memory growth needs to be the same across GPUs.
 			for gpu in gpus:
 				tf.config.experimental.set_memory_growth(gpu, True)
 
-			#use only the selcted gpu
+			# Use only the selcted gpu.
 			tf.config.experimental.set_visible_devices(gpus[gpu_id], 'GPU')
 		except RuntimeError as e:
-			# Visible devices must be set before GPUs have been initialized
+			# Visible devices must be set before GPUs have been initialized.
 			print(e)
 
 		logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-		print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+		print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
 
 
 def create_callbacks(
@@ -65,28 +76,20 @@ def create_callbacks(
 ):
 	callbacks = []
 
-	# save snapshots of the model
-	if not config['snapshots_path']:
-		from pathlib import Path
-		home = str(Path.home())
-		config['snapshots_path'] = os.path.join(home, 'retinanet-snapshots')
-	if not config['project_name']:
-		from datetime import datetime
-		config['project_name'] = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
-
+	# Save snapshots of the model.
 	makedirs(os.path.join(config['snapshots_path'], config['project_name']))
 	checkpoint = tf.keras.callbacks.ModelCheckpoint(
 		os.path.join(
 			config['snapshots_path'],
 			config['project_name'],
-			'{{epoch:02d}}.h5'.format()
+			'{epoch:02d}.h5'
 		),
 		verbose=1,
 	)
 	checkpoint = RedirectModel(checkpoint, model)
 	callbacks.append(checkpoint)
 
-	# evaluate the model
+	# Evaluate the model.
 	if validation_generator:
 		print('Validation will be implemented.')
 
@@ -94,16 +97,16 @@ def create_callbacks(
 
 
 def main():
-	# parse the configuration file
+	# Parse the configuration file.
 	config = parse_yaml()
 
-	# set gpu configuration
+	# Set gpu configuration.
 	setup_gpu(config['train']['gpu'])
 
-	# get the backbone
+	# Get the backbone.
 	backbone = get_backbone(config)
 
-	# get the generators
+	# Get the generators.
 	generators = get_generators(
 		config,
 		preprocess_image=backbone.preprocess_image
@@ -119,12 +122,12 @@ def main():
 	if 'custom_evaluation' in generators:
 		evaluation = generators['custom_evaluation']
 
-	# create the models
+	# Create the models.
 	model            = backbone.retinanet(train_generator.num_classes())
 	training_model   = model
 	prediction_model = models.retinanet.retinanet_bbox(training_model)
 
-	# create the callbacks
+	# Create the callbacks.
 	callbacks = create_callbacks(
 		config['callbacks'],
 		model,
@@ -134,10 +137,10 @@ def main():
 		evaluation,
 	)
 
-	# print model
+	# Print model.
 	print(training_model.summary())
 
-	# compile model
+	# Compile model.
 	training_model.compile(
 		loss={
 			'regression'    : losses.smooth_l1(),
@@ -146,16 +149,10 @@ def main():
 		optimizer=tf.keras.optimizers.Adam(lr=1e-5, clipnorm=0.001)
 	)
 
-	# parse training parameters
+	# Parse training parameters.
 	train_config = config['train']
 
-	# Use multiprocessing if workers > 0
-	if train_config['workers'] > 0:
-		use_multiprocessing = True
-	else:
-		use_multiprocessing = False
-
-	# start training
+	# Start training.
 	return training_model.fit_generator(
 		generator=train_generator,
 		steps_per_epoch=train_config['steps_per_epoch'],
@@ -163,7 +160,7 @@ def main():
 		verbose=1,
 		callbacks=callbacks,
 		workers=train_config['workers'],
-		use_multiprocessing=use_multiprocessing,
+		use_multiprocessing=train_config['use_multiprocessing'],
 		max_queue_size=train_config['max_queue_size'],
 	)
 
