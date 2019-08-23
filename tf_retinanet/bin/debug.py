4aeca2e45737c5e601992529dbac3664e6497c87
Hans Gaiser <h.gaiser@fizyr.com>
2019-08-23 14:53:57 +0200

    Update copyright year.

--- tf_retinanet/bin/debug.py
+++ tf_retinanet/bin/debug.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Copyright 2017-2018 Fizyr (https://fizyr.com)
+Copyright 2017-2019 Fizyr (https://fizyr.com)
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
