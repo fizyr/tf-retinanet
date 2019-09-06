ab7538805953c56bfefd07e4f227ace041dbc0fd
vcarpani <v.carpani@fizyr.com>
2019-09-06 15:04:34 +0200

    Update resize to tf2.

--- tf_retinanet/layers/upsample_like.py
+++ tf_retinanet/layers/upsample_like.py
@@ -26,11 +26,11 @@ class UpsampleLike(tf.keras.layers.Layer):
 		target_shape = tf.keras.backend.shape(target)
 		if tf.keras.backend.image_data_format() == 'channels_first':
 			source = tf.transpose(source, (0, 2, 3, 1))
-			output = tf.compat.v1.image.resize(source, (target_shape[2], target_shape[3]), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+			output = tf.image.resize(source, (target_shape[2], target_shape[3]), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 			output = tf.transpose(output, (0, 3, 1, 2))
 			return output
 		else:
-			return tf.compat.v1.image.resize(source, (target_shape[1], target_shape[2]), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+			return tf.image.resize(source, (target_shape[1], target_shape[2]), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 
 	def compute_output_shape(self, input_shape):
 		if tf.keras.backend.image_data_format() == 'channels_first':
