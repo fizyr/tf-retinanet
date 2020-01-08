664b97b2727db58375bfedebcee7e32b248fcb40
Enrico Liscio <e.liscio@fizyr.com>
2020-01-08 16:08:45 +0100

    Add optional additional_objects in load_model function.

--- tf_retinanet/models/__init__.py
+++ tf_retinanet/models/__init__.py
@@ -1,5 +1,5 @@
 """
-Copyright 2017-2019 Fizyr (https://fizyr.com)
+Copyright 2017-2020 Fizyr (https://fizyr.com)
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,19 +18,19 @@ from . import retinanet  # noqa: F401
 from . import submodels  # noqa: F401
 
 
-def load_model(filepath, backbone, submodels):
+def load_model(filepath, backbone, submodels, additional_objects=None):
 	""" Loads a retinanet model using the correct custom objects.
 	Args
-		filepath: one of the following:
+		filepath : one of the following:
 			- string, path to the saved model, or
 			- h5py.File object from which to load the model
-		backbone: Backbone with which the model was trained.
-		submodels: List of submodels the .
+		backbone  : Backbone with which the model was trained.
+		submodels : List of submodels used in the model.
 	Returns
 		A tf.keras.models.Model object.
 	Raises
-		ImportError: if h5py is not available.
-		ValueError: In case of an invalid savefile.
+		ImportError : if h5py is not available.
+		ValueError  : In case of an invalid savefile.
 	"""
 	import tensorflow as tf
 	custom_objects = backbone.custom_objects
@@ -43,6 +43,10 @@ def load_model(filepath, backbone, submodels):
 			print('WARNING: custom object present in multiple dicts:', key)
 
 		# Merge the dicts.
-		custom_objects = {**custom_objects, **submodel_custom_objects}
+		custom_objects.update(submodel_custom_objects)
+
+	# Integrate with additional objects, if provided.
+	if additional_objects:
+		custom_objects.update(additional_objects)
 
 	return tf.keras.models.load_model(filepath, custom_objects=custom_objects)
