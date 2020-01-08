0c2cdc7f9e231c68b7dbe4423cfcc349d34030eb
Enrico Liscio <e.liscio@fizyr.com>
2020-01-08 16:08:45 +0100

    Add optional custom_objects in load_model function.

--- tf_retinanet/models/__init__.py
+++ tf_retinanet/models/__init__.py
@@ -1,5 +1,5 @@
 """
-Copyright 2017-2019 Fizyr (https://fizyr.com)
+Copyright 2017-2020 Fizyr (https://fizyr.com)
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,31 +18,31 @@ from . import retinanet  # noqa: F401
 from . import submodels  # noqa: F401
 
 
-def load_model(filepath, backbone, submodels):
+def load_model(filepath, backbone, submodels, custom_objects=None):
 	""" Loads a retinanet model using the correct custom objects.
 	Args
-		filepath: one of the following:
+		filepath : one of the following:
 			- string, path to the saved model, or
 			- h5py.File object from which to load the model
-		backbone: Backbone with which the model was trained.
-		submodels: List of submodels the .
+		backbone       : Backbone with which the model was trained.
+		submodels      : List of submodels used in the model.
+		custom_objects : Optional dictionary of custom objects to be passed while loading the model.
 	Returns
 		A tf.keras.models.Model object.
 	Raises
-		ImportError: if h5py is not available.
-		ValueError: In case of an invalid savefile.
+		ImportError : if h5py is not available.
+		ValueError  : In case of an invalid savefile.
 	"""
 	import tensorflow as tf
-	custom_objects = backbone.custom_objects
+
+	if custom_objects is None:
+		custom_objects = {}
+
+	# Update custom_objects with the backbone custom objects.
+	custom_objects.update(backbone.custom_objects)
+
+	# Update custom_objects with the custom objects of each submodel.
 	for submodel in submodels:
-		# Get the custom object of the submodel.
-		submodel_custom_objects = submodel.get_custom_objects()
-
-		# Check if the custom object is alredy present.
-		for key in set(custom_objects).intersection(submodel_custom_objects):
-			print('WARNING: custom object present in multiple dicts:', key)
-
-		# Merge the dicts.
-		custom_objects = {**custom_objects, **submodel_custom_objects}
+		custom_objects.update(submodel.get_custom_objects())
 
 	return tf.keras.models.load_model(filepath, custom_objects=custom_objects)
